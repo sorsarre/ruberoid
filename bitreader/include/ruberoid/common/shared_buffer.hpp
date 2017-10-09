f4a05dadf61ed860d27aead0f64d404f24df2ed1
Andrey Kaydalov <sorsarre@gmail.com>
2017-10-09 22:01:55 +0300

    Shared buffer tests and fixes

--- bitreader/include/ruberoid/common/shared_buffer.hpp
+++ bitreader/include/ruberoid/common/shared_buffer.hpp
@@ -11,13 +11,15 @@ namespace rb::common
     {
     public:
         //----------------------------------------------------------------------
-        shared_buffer(shared_buffer&& other) noexcept: _state(std::move(other._state))
+        shared_buffer(): _state(make_internal(_internal{}))
         {
 
         }
 
         //----------------------------------------------------------------------
+        shared_buffer(shared_buffer&& other) = default;
         shared_buffer(const shared_buffer& other) = default;
+        shared_buffer& operator=(const shared_buffer& other) = default;
         ~shared_buffer() = default;
 
         //----------------------------------------------------------------------
@@ -39,58 +41,85 @@ namespace rb::common
         //----------------------------------------------------------------------
         static shared_buffer wrap_mem(uint8_t* data, size_t size)
         {
-            _internal state = {
-                .data = data,
-                .size = size,
-                .capacity = size
-            };
-
-            return shared_buffer(state);
+            return shared_buffer(make_internal(data, size, size));
         }
 
         //----------------------------------------------------------------------
         static shared_buffer copy_mem(const uint8_t* data, size_t size)
         {
-            _internal state = {
-                .data = new uint8_t[size],
-                .size = size,
-                .capacity = size
-            };
-
-            return shared_buffer(state);
+            auto data_copy = new uint8_t[size];
+            std::copy(data, data+size, data_copy);
+            return shared_buffer(make_internal(data_copy, size, size));
         }
 
         //----------------------------------------------------------------------
         static shared_buffer clone(const shared_buffer& buffer)
         {
-            _internal state = {
-                .data = new uint8_t[buffer.capacity()],
-                .size = buffer.size(),
-                .capacity = buffer.capacity()
-            };
-            std::copy(buffer.get(), buffer.get()+buffer.size(), state.data);
+            if (!buffer.get()) {
+                return shared_buffer(_internal{});
+            }
+
+
+            auto state = make_internal(
+                    new uint8_t[buffer.capacity()],
+                    buffer.size(),
+                    buffer.capacity()
+            );
+
+            std::copy(buffer.get(), buffer.get()+buffer.size(), state->data);
             return shared_buffer(state);
+        }
+
+        //----------------------------------------------------------------------
+        static shared_buffer allocate(size_t size)
+        {
+            if (size == 0) {
+                return shared_buffer();
+            } else {
+                return shared_buffer(make_internal(new uint8_t[size], 0, size));
+            }
+        }
+
+        //----------------------------------------------------------------------
+        void realloc(size_t new_size)
+        {
+            if (new_size == capacity()) {
+                return;
+            } else if (new_size == 0) {
+                _state = make_internal(_internal{});
+            } else {
+                auto new_data = new uint8_t[new_size];
+                auto copy_size = std::min(size(), new_size);
+                if (copy_size > 0) {
+                    std::copy(begin(), begin()+copy_size, new_data);
+                }
+
+                _state = make_internal(new_data, copy_size, new_size);
+            }
         }
 
         //----------------------------------------------------------------------
         void resize(size_t new_size)
         {
-            auto new_data = new uint8_t[new_size];
-            auto copy_size = std::min(new_size, size());
-            std::copy(begin(), begin()+copy_size, new_data);
-            _internal state = {
-                .data = new_data,
-                .size = new_size,
-                .capacity = new_size
-            };
+            if (new_size == size()) {
+                return;
+            } else if (new_size <= capacity()) {
+                _state->size = new_size;
+            } else {
+                auto new_data = new uint8_t[new_size];
+                auto copy_size = std::min(new_size, size());
+                if (copy_size > 0) {
+                    std::copy(begin(), begin()+copy_size, new_data);
+                }
 
-            _state = make_internal(state);
+                _state = make_internal(new_data, new_size, new_size);
+            }
         }
 
         //----------------------------------------------------------------------
-        bool operator bool() const
+        operator bool() const
         {
-            return _state.get();
+            return (_state->data != nullptr);
         }
 
     private:
@@ -106,7 +135,7 @@ namespace rb::common
         static std::shared_ptr<_internal> make_internal(_internal state)
         {
             std::shared_ptr<_internal> ret(
-                new _internal(),
+                new _internal(state),
                 [](_internal* ptr) {
                     delete[] ptr->data;
                     ptr->size = 0;
@@ -118,7 +147,23 @@ namespace rb::common
         }
 
         //----------------------------------------------------------------------
+        static std::shared_ptr<_internal> make_internal(uint8_t* data, size_t size, size_t capacity)
+        {
+            _internal state;
+            state.data = data;
+            state.size = size;
+            state.capacity = capacity;
+            return make_internal(state);
+        }
+
+        //----------------------------------------------------------------------
         explicit shared_buffer(_internal state): _state(make_internal(state))
+        {
+
+        }
+
+        //----------------------------------------------------------------------
+        explicit shared_buffer(std::shared_ptr<_internal> state): _state(std::move(state))
         {
 
         }
